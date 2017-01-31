d8172d1dadd3ab316dc9151a64609148b63922cc
rabindranathmanki <rabindranath436@gmail.com>
2017-01-31 23:35:37 +0530

    my first commit

---
+++ percentage.py
@@ -0,0 +1,12 @@
+import sys
+per=input("enter your percentage")
+if per>=80:
+        print("first division honors")
+elif per>=60:
+	print("first division")
+elif per>=50:
+	print("second division")
+elif per>=40:
+	print("third division ")
+else:
+	print"fail"

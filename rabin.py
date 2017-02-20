b06f52b8b0a57ab8f30eab2e4ae2aab6a4f1af4f
Rabindra Nath Manki <rabindranath436@gmail.com>
2017-02-20 08:07:53 +0000

    Add files via upload

---
+++ rabin.py
@@ -0,0 +1,11 @@
+while 1:
+    option='y'
+    while option.upper()=='Y':
+        precentage=input("enter your precentage")
+        if precentage>=60:
+            print "you are selected"
+        else:
+            print "you are not selected"
+
+        
+
